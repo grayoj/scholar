9b994a2d0b8c14b4d1405f67443940b1367c3350
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-30 14:03:22 +0100

    Stage Student Model.

---
+++ server/src/main/java/model/Student.java
@@ -0,0 +1,19 @@
+/**
+ * Gerald Maduabuchi
+ * Student Model
+ */
+
+ // Package
+package model;
+
+// Student Class Function
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String className;
+    private String phone;
+    private String imageLoc;
+    private String email;
+    private String employeeCode;
+}
