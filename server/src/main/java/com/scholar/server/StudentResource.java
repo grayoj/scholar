854e139ddd437f457eb4be64b45226f1dadd645d
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-30 16:07:17 +0100

    Stage Student Resource Controller.

--- server/src/main/java/com/scholar/server/StudentResource.java
+++ server/src/main/java/com/scholar/server/StudentResource.java
@@ -1,5 +1,22 @@
+/**
+ * Gerald Maduabuchi
+ * Student Resource
+ * The API Controller
+ */
 package com.scholar.server;
 
+import com.scholar.server.service.StudentService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentResource {
-    
+    private final StudentService studentService;
+
+    public StudentResource(StudentService studentService) {
+        this.studentService = studentService;
+        
+    }
 }
