384e22aa58782b52e1d162e3ba633be37304d2b3
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-30 15:14:03 +0100

    Implement Add Student Function.

--- server/src/main/java/com/scholar/server/service/StudentService.java
+++ server/src/main/java/com/scholar/server/service/StudentService.java
@@ -4,6 +4,9 @@
  */
 package com.scholar.server.service;
 
+import java.util.UUID;
+
+import com.scholar.server.model.Student;
 import com.scholar.server.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +19,11 @@ public class StudentService {
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
+    }
+
+    // Add Student
+    public Student addStudent(Student student) {
+        student.setStudentCode(UUID.randomUUID().toString());
+        return studentRepository.save(student);      
     }
 }
