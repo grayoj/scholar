01e5e76a727c5c1c3ddf23c64e8fa484fd12430f
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-30 15:19:22 +0100

    Update Student Repository Delete Method.

--- server/src/main/java/com/scholar/server/repository/StudentRepository.java
+++ server/src/main/java/com/scholar/server/repository/StudentRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    void deleteStudentById(Long id);
+
 }
