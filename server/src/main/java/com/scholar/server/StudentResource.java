271f40957825ca1da2c9f602644285ab53588c7f
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-31 08:18:22 +0100

    Update Student Resource.

--- server/src/main/java/com/scholar/server/StudentResource.java
+++ server/src/main/java/com/scholar/server/StudentResource.java
@@ -5,8 +5,15 @@
  */
 package com.scholar.server;
 
+import java.util.List;
+
+import com.scholar.server.model.Student;
 import com.scholar.server.service.StudentService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +25,17 @@ public class StudentResource {
     public StudentResource(StudentService studentService) {
         this.studentService = studentService;
         
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.findAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") Long id) {
+        Student student = studentService.findStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 }
