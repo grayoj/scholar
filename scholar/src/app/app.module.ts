32e471679b2c20070183b776cb56bf29731bbf02
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-31 13:48:26 +0100

    Update HttpClientModule

--- scholar/src/app/app.module.ts
+++ scholar/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,7 +10,8 @@ import { StudentService } from './student.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [StudentService],
   bootstrap: [AppComponent]
