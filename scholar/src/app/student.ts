f2907211e4e3b9917b2a3763c4f3be0733e176a1
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-31 10:39:13 +0100

    Export Student Interface Props.

---
+++ scholar/src/app/student.ts
@@ -0,0 +1,11 @@
+// Export props
+export interface Student {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  className: string;
+  phone: string;
+  imageLoc: string;
+  studentCode: string;
+}
