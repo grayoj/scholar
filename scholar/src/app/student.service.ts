b6073950a6bf0fb9ac552a3fdd4482638daec7e4
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-31 10:40:33 +0100

    Implement Student Method.

--- scholar/src/app/student.service.ts
+++ scholar/src/app/student.service.ts
@@ -8,6 +8,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import  { Observable } from 'rxjs';
 
+// Import Student module
+import { Student } from './student';
+
 // Inject API consumed.
 @Injectable({
   providedIn: 'root'
@@ -18,7 +21,8 @@ export class StudentService {
 
   constructor(private http: HttpClient) {}
 
-  public getStudents(): Observable<any> {
+  public getStudents(): Observable<Student[]> {
     return this.http.get<any>(`${this.apiServerUrl}/student/all`);
   }
+
 }
