83b323b1f831c5f773d17f55f2d9ac5b96915e58
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-31 08:23:30 +0100

    Post Student Method.

--- server/src/main/java/com/scholar/server/StudentResource.java
+++ server/src/main/java/com/scholar/server/StudentResource.java
@@ -14,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +41,9 @@ public class StudentResource {
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<Student> addStudent(@RequestBody Student student) {
+        Student newStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(newStudent, HttpStatus.CREATED);
+    }
 }
