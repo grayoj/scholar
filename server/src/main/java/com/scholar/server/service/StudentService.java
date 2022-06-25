106b96437541cd9559054958e7a50cab22bd096b
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-06-25 12:29:36 +0100

    Update Server Application

--- server/src/main/java/com/scholar/server/service/StudentService.java
+++ server/src/main/java/com/scholar/server/service/StudentService.java
@@ -13,7 +13,9 @@ import com.scholar.server.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
